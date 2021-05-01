3d4b2a7aceab55df1e7c392c77ac61cfb10e76ce
lengkubpome <lengkubpome@gmail.com>
2021-05-02 00:21:38 +0700

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+
+const routes: Routes = [
+	{
+		path: 'pages',
+		loadChildren: () =>
+			import('./pages/pages.module').then((m) => m.PagesModule)
+	},
+	{ path: '', redirectTo: 'pages', pathMatch: 'full' },
+	{ path: '**', redirectTo: 'pages' }
+];
+
+const config: ExtraOptions = {
+	useHash: false
+};
+
+@NgModule({
+	imports: [ RouterModule.forRoot(routes, config) ],
+	exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
+import { ThemeModule } from './@theme/theme.module';
+import { CoreModule } from './@core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+	NbThemeModule,
+	NbLayoutModule,
+	NbSidebarModule,
+	NbMenuModule,
+	NbDatepickerModule,
+	NbDialogModule,
+	NbWindowModule,
+	NbToastrModule
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent ],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		NbThemeModule.forRoot({ name: 'default' }),
+		NbLayoutModule,
+		NbEvaIconsModule,
+		AppRoutingModule,
+		NbLayoutModule,
+		NbSidebarModule.forRoot(),
+		NbMenuModule.forRoot(),
+		NbDatepickerModule.forRoot(),
+		NbDialogModule.forRoot(),
+		NbWindowModule.forRoot(),
+		NbToastrModule.forRoot(),
+		CoreModule.forRoot(),
+		ThemeModule.forRoot()
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
