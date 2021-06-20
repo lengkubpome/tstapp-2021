638b479d73358a90cb4cf1cbe1e0993459bf83c3
lengkubpome <lengkubpome@gmail.com>
2021-06-20 23:04:39 +0700

    เพิ่มเติม script service

---
+++ src/app/shared/services/script.store.ts
@@ -0,0 +1,8 @@
+interface Scripts {
+	name: string;
+	src: string;
+}
+export const ScriptStore: Scripts[] = [
+	{ name: 'lineLiffV2', src: 'https://static.line-scdn.net/liff/edge/2/sdk.js' }
+	// { name: 'rangeSlider', src: '../../../assets/js/ion.rangeSlider.min.js' }
+];
